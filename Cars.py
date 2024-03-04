9bd94eee8824bdd35b372a656f7aeecdb39c4bee
TommyGun10 <161822959+TommyGun10@users.noreply.github.com>
2024-03-04 16:14:13 +0700

    Add files via upload

---
+++ Cars.py
@@ -0,0 +1,8 @@
+
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in list_:
+    print('Я езжу на автомабиле марки', i)
+cars_count = 0
+for j in list_:
+    cars_count += 10
+print(cars_count)
