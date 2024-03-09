b8064b624991902fe2c36c46bafaf97a19b7a637
TommyGun10 <161822959+TommyGun10@users.noreply.github.com>
2024-03-09 12:58:14 +0700

    Add files via upload

---
+++ func.py
@@ -0,0 +1,8 @@
+def print_params(cat1='Murzik', cat2='Tishka'):
+    print(cat1, cat2)
+
+
+print_params()
+print_params(cat1='hungry', cat2='full')
+print_params()
+print_params(cat1='angry', cat2='kind')
