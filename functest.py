3e68caac9519ba2ea2c018784f47049b27ba8181
TommyGun10 <161822959+TommyGun10@users.noreply.github.com>
2024-03-13 14:44:09 +0700

    Add files via upload

---
+++ functest.py
@@ -0,0 +1,21 @@
+def test():
+    a = 'smth'
+    b = 'anr'
+    print(a, b)
+
+
+def test2(param1, param2, param3):
+    print(param1, param2, param3)
+
+
+test()
+test2(param1=1, param2=2, param3=3)
+
+
+
+
+
+
+
+
+
