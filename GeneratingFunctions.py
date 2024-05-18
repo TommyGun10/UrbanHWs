88c1cce936c4f9ebd37d0283ef18517ec8026d46
TommyGun10 <161822959+TommyGun10@users.noreply.github.com>
2024-05-18 14:36:47 +0700

    Add files via upload

---
+++ GeneratingFunctions.py
@@ -0,0 +1,29 @@
+def func_fabric(x, y):  # Фабрика функций
+    if x > y:
+        return x / y
+    elif x == y:
+        return 1
+    else:
+        return 0
+print(func_fabric(1, 2))
+
+
+square = lambda i: i ** 2 # Лямбда - функции, с аналогом def()
+print(square(5))
+
+
+def square_def(i):
+    return i ** 2
+print(square_def(5))
+
+
+class Rect:
+    def __init__(self, a):
+        self.a = a
+
+    def __call__(self, b):
+        return b * self.a
+
+side = Rect(a=15)
+result = side(b=20)
+print(result)
