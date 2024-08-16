a573e89d69e24fb77e63064d27a4938b914257e6
TommyGun10 <161822959+TommyGun10@users.noreply.github.com>
2024-08-16 17:46:46 +0700

    Update BotUpgrade.py

--- BotUpgrade.py
+++ BotUpgrade.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-api = "7429366363:AAHxWZzj-M-sypwgulHAZ4i-rQWGpUGcg7w"
+api = "YOUR API"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
